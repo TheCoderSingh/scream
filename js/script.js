0a062a06843081000b99ac39eb27626e41d23863
Jaskaran Singh <jaskaran1523@gmail.com>
2019-07-29 10:38:30 -0700

    fix: Fix JS errors and styles

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ reg1 = () => {
 	regsubmit.addEventListener("click", (e) => {
 		e.preventDefault();
 
-		if (regemail.value === "" || regpass.value === "" || regconfpass.value === "") {
+		if (regemail.value === "" || regpass.value === "" || regconfpass.value === "")
 			regform1.innerHTML += "<p class='error' style='color:#FFF; font-family: Roboto, sans-serif; font-size: 0.8em'>Please complete all the information.</p>";
-		}
-		if (regpass.value !== regconfpass.value) {
+
+		if (regpass.value !== regconfpass.value)
 			regform1.innerHTML += "<p class='error' style='color:#FFF; font-family: Roboto, sans-serif; font-size: 0.8em'>Passwords do not match.</p>";
-		}
+
 		let form = document.getElementsByName("regform1")[0];
 		form.submit();
 	});
@@ -17,8 +17,8 @@ reg2 = () => {
 	reg2submit.addEventListener("click", (e) => {
 		e.preventDefault();
 
-		if (regpass.value !== regconfpass.value)
-			regform2.innerHTML += "<p class='error' style='color: red';>Passwords do not match</p>";
+		if (firstname.value === "" || lastname.value === "" || username.value === "")
+			regform2.innerHTML += "<p class='error' style='color:#FFF; font-family: Roboto, sans-serif; font-size: 0.8em'>Please complete all the information.</p>";
 	});
 };
 
