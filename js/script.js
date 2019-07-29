7c294cdd1656bd6acf77d0d391984b7592701e95
Jaskaran Singh <jaskaran1523@gmail.com>
2019-07-29 09:43:40 -0700

    fix: Change functions to onload and fixed styles

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
-let x = document.getElementById("regsubmit");
-x.addEventListener("click", (e) => {
-	e.preventDefault();
+reg1 = () => {
+	regsubmit.addEventListener("click", (e) => {
+		e.preventDefault();
 
-	if (regemail.value === "" || regpass.value === "" || regconfpass.value === "") {
-		regform1.innerHTML += "<p class='error'>Please complete all the information.</p>";
-	}
-	if (regpass.value !== regconfpass.value) {
-		regform1.innerHTML += "<p class='error'>Passwords do not match</p>";
-	}
-	let form = document.getElementsByName("regform1")[0];
-	form.submit();
-});
+		if (regemail.value === "" || regpass.value === "" || regconfpass.value === "") {
+			regform1.innerHTML += "<p class='error' style='color:#FFF; font-family: Roboto, sans-serif; font-size: 0.8em'>Please complete all the information.</p>";
+		}
+		if (regpass.value !== regconfpass.value) {
+			regform1.innerHTML += "<p class='error' style='color:#FFF; font-family: Roboto, sans-serif; font-size: 0.8em'>Passwords do not match.</p>";
+		}
+		let form = document.getElementsByName("regform1")[0];
+		form.submit();
+	});
+};
 
-reg2submit.addEventListener("click", (e) => {
-	e.preventDefault();
+reg2 = () => {
+	reg2submit.addEventListener("click", (e) => {
+		e.preventDefault();
 
-	if (regpass.value !== regconfpass.value)
-		regform2.innerHTML += "<p class='error'>Passwords do not match</p>";
-});
+		if (regpass.value !== regconfpass.value)
+			regform2.innerHTML += "<p class='error' style='color: red';>Passwords do not match</p>";
+	});
+};
 
-msgbtn.addEventListener("click", (e) => {
-	e.preventDefault();
+sendmsg = () => {
+	msgbtn.addEventListener("click", (e) => {
+		e.preventDefault();
 
-	let today = new Date();
+		let today = new Date();
 
-	let msg = inpmsg.value;
+		let msg = inpmsg.value;
 
-	if (msg !== "") {
-		messagearea.innerHTML += `<div class='msg'><p class='msgtime' > ${today.getHours()}:${today.getMinutes()}</p ><div class='ownmsgcontent'><p class='msgtext'> ${msg} </p><img src='images/face.jpeg' alt='Sender Image' class='senderimg'></div></div>`;
+		if (msg !== "") {
+			messagearea.innerHTML += `<div class='msg'><p class='msgtime' > ${today.getHours()}:${today.getMinutes()}</p ><div class='ownmsgcontent'><p class='msgtext'> ${msg} </p><img src='images/face.jpeg' alt='Sender Image' class='senderimg'></div></div>`;
 
-		inpmsg.value = null;
-	}
-});
+			inpmsg.value = null;
+		}
+	});
+};
